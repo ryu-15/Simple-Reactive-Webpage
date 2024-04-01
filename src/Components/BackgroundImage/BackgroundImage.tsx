2749f5f9d99d58e45da7ce84c439d9f4d9b7ae80
ryu-15 <meghagrg15@gmail.com>
2024-04-01 12:32:31 +0545

    update:moved img file to public folder

--- src/Components/BackgroundImage/BackgroundImage.tsx
+++ src/Components/BackgroundImage/BackgroundImage.tsx
@@ -6,11 +6,11 @@ const BackgroundSection=()=>{
             <img src="/src/assets/BackgroundShape/SmallCircle.jpg" alt="Small Circle" className="background-small-1-ellipse"/>
             <img src="/src/assets/BackgroundShape/SmallCircle.jpg" alt="Small Circle" className="background-small-2-ellipse"/>
             <img src="/src/assets/BackgroundShape/SmallCircle.jpg" alt="Small Circle" className="background-small-3-ellipse"/>
-            <img src="/src/assets/BackgroundImg/Revel-logo.jpg" alt="revel logo" className="revel-logo-img"/>
-            <img src="/src/assets/BackgroundImg/ui-ux.jpg" alt="ui/ux" className="ui-ux-img"/>
+            <img src="/BackgroundImg/Revel-logo.jpg" alt="revel logo" className="revel-logo-img"/>
+            <img src="/BackgroundImg/ui-ux.jpg" alt="ui/ux" className="ui-ux-img"/>
             <img src="/src/assets/BackgroundShape/hollow.jpg" alt="hollow" className="hollow-circle" />
             <div className="background-robot-img-wrapper">
-                <img src="/src/assets/BackgroundImg/robot.jpg" alt="robot image" className="background-robot-img"/>
+                <img src="/BackgroundImg/robot.jpg" alt="robot image" className="background-robot-img"/>
             </div>
         </div>
     )
